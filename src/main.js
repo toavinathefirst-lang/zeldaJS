c9410400c706443be3040c7a146e4e589b7c5f19
toavinathedev-luffy <toavinathefirst@gmail.com>
2026-08-11 23:26:26 +0300

    initialisation de la map et des constante globale

--- src/main.js
+++ src/main.js
@@ -1,3 +1,60 @@
 import './style.css'
 
+document.addEventListener('DOMContentLoaded',()=>{
+    const grid = document.getElementById("grid");
+    const scoreDisplay  =  document.getElementById("score");
+    const levelDisplay = document.getElementById("level");
+    const enemyDisplay = document.getElementById("enemies");
+
+    
+    const width = 10
+    const tileSize = 48
+
+    const squares = []
+    let score = 0
+    let level = 0
+    let playerPosition = 40
+    let enemies = []
+    let playerDirection = 'right'
+    let gameRunning = true
+
+    //y,w,x,z = corner walls | a,b=side walls |c,d =top/bottom walls
+    //)=lanterns |=(=fire pot | %=left door  | ^=top door | $=stairs
+    //*=slicer enemy  | }=skeletor enemy | (space) =empty walkable area
+
+   const maps = [
+        // Level 1 layout
+        [
+            'ycc)cc^ccw',
+            'a        b',
+            'a      * b',
+            'a    (   b',
+            '%        b',
+            'a    (   b',
+            'a  *     b',
+            'a        b',
+            'xdd)dd)ddz'
+        ],
+        // Level 2 layout
+        [
+            'yccccccccw',
+            'a        b',
+            ')        )',
+            'a        b',
+            'a        b',
+            'a    $   b',
+            ')   }    )',
+            'a        b',
+            'xddddddddz',
+        ]
+    ]
+    function createBoard(){
+        gameRunning = true;
+        grid.innerHTML="";
+        squares.length=0;
+        enemies=[]
+        for (let i = 0; i < 9; i++) {
+        }
+    }
+})
 
